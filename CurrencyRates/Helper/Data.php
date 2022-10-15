b666637e81ad9b6aa51e92da7d1e43fb6bea3707
PaWuK11 <buchulyakp10@gmail.com>
2022-10-15 16:10:38 +0300

    Add Data on Folder Helper

---
+++ CurrencyRates/Helper/Data.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace ELogic\CurrencyRates\Helper;
+
+
+
+use ELogic\CurrencyRates\Api\CurrencyRateReposytoryInterface;
+use ELogic\CurrencyRates\Model\CurrencyRate;
+use Magento\CatalogGraphQl\DataProvider\Product\SearchCriteriaBuilder;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\SearchCriteria;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\ObjectManager\Profiler\Code\Generator\Logger;
+use Magento\Tests\NamingConvention\true\string;
+
+class Data extends AbstractHelper
+{
+    const BASE_CURRENCY = 'USD';
+    const CURRENCY_FROM = 'currency_from';
+    const CURRENCY_TO = 'currency_to';
+
+    private $currencyRateFactory;
+    private $currencyRateRepository;
+    private $searchCriteriaBuilder;
+    private $filterBuilder;
+    private $logger;
+
+    public function __construct(
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        FilterBuilder $filterBuilder,
+        CurrencyRateReposytoryInterface $repository,
+        CurrencyRateFactory $currencyRateFactory,
+        Logger $logger,
+        Context $context
+    )
+    {
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->filterBuilder = $filterBuilder;
+        $this->currencyRateFactory = $currencyRateFactory;
+        $this->currencyRateRepository = $repository;
+        $this->logger = $logger;
+        parent::__construct($context);
+    }
+
+    public function getFirstPriceRate(string $currency, string $baseCurrency = self::BASE_CURRENCY)
+    {
+        $result = null;
+        $filters[] = $this->filterBuilder->setField(self::CURRENCY_TO)
+            ->setConditionType('eq')
+            ->setValue($currency)
+            ->create();
+        $this->searchCriteriaBuilder->addFilters($filters);
+
+        $searchCriteria = $this->searchCriteriaBuilder->create();
+        try {
+            $result = $this->currencyRateRepositoryg->getList($searchCriteria);
+        } catch (LocalizedException $e)
+        {
+            $this->logger->debug((string)$e);
+        }
+
+        $items = $result->getItems();
+
+        foreach ($items as $item){
+            $result = $item->getPriceMatrixCurrencyRate();
+        }
+        return $result;
+    }
+
+    public function getSecondPriceRate(string $currency, string $baseCurrency = self::BASE_CURRENCY)
+    {
+        $result = null;
+        $filters[] = $this->filterBuilder->setField(self::CURRENCY_TO)
+            ->setConditionType('eq')
+            ->setValue($currency)
+            ->create();
+        $this->searchCriteriaBuilder->addFilters($filters);
+
+        $searchCriteria = $this->searchCriteriaBuilder->create();
+        try {
+            $result = $this->currencyRateRepositoryg->getList($searchCriteria);
+        } catch (LocalizedException $e)
+        {
+            $this->logger->debug((string)$e);
+        }
+
+        $items = $result->getItems();
+
+        foreach ($items as $item){
+            $result = $item->getPriceMatrixCurrencyRate();
+        }
+        return $result;
+    }
+}
